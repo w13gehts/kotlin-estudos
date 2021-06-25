0723fb9cddf93922650a02054982bb105de02edf
Juliana <juliana.salafia@gmail.com>
2021-06-24 21:43:29 -0300

    Preparação do projeto do curso inicial sobre Kotlin

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main () {
+    println("Bem-vindo ao Bytebank")
+
+    val titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular $titular")
+    println("Conta $numeroConta")
+    println("Saldo $saldo")
+}
+
